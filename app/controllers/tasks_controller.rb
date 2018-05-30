0628f0b35f7b40d1dff2c1742bbde6072e3ee0f6
Sadohara Hirokazu <sadohara.hirokazu@adways.net>
2018-05-30 15:48:20 +0900

    タスクの登録・表示処理の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,29 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
   def list
     @task = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
+  def create
+    binding.pry
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:content)
+    end
+
 end
